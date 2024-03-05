a7a28ffedebb320f201e19aae825de799094fbbd
NykolaiPerezUNLaSMN <nykolaiperez@gmail.com>
2024-03-05 16:01:22 -0300

    Ejemplo 1 Concurrencia - Hilos con Joins

---
+++ Ejemplos_Basicos_Hilos/src/Ejemplos/Hilos2.java
@@ -0,0 +1,58 @@
+package Ejemplos;
+
+///Primero heredar la clase Hilos2 Thread
+public class Hilos2  extends Thread{
+	
+	//Lea grego un atributo cualquiera a mi clase Hilos2
+	public int numero; //identificador del hilo
+	
+	public void run() {
+		System.out.println("Soy el hilo: " +numero);
+	}
+	
+	
+	
+	//Obviamente genero su contructor, hasta acá todo igual
+	public Hilos2(int numero) {
+		this.numero = numero;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Hilos2 [numero=" + numero + "]";
+	}
+
+
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Lo mismo de antes pero para un array
+		Hilos2[]  vectorDeHilos= new Hilos2[5];
+		
+		///Instancio cada posición del array
+		for ( int i =0 ; i<vectorDeHilos.length; i++) {
+			
+			vectorDeHilos[i] = new Hilos2(i); 
+			//Instancio cada elemento del array
+			
+		}
+		
+		///Corro hilos para cara pos del vector, OBvio que se podria hacer en 
+		//el mismo for
+		for ( int i =0 ; i<vectorDeHilos.length; i++) {
+			
+			vectorDeHilos[i].start();
+			
+		}
+		
+		
+		System.out.println("Soy el main!!!");
+		
+
+	}
+
+}
---
+++ Ejemplos_Basicos_Hilos/src/Ejemplos/Hilos3.java
@@ -0,0 +1,69 @@
+package Ejemplos;
+
+///Primero heredar la clase Hilos2 Thread
+public class Hilos3  extends Thread{
+	
+	//Lea grego un atributo cualquiera a mi clase Hilos2
+	public int numero; //identificador del hilo
+	
+	public void run() {
+		System.out.println("Soy el hilo: " +numero);
+	}
+	
+	
+	
+	//Obviamente genero su contructor, hasta acá todo igual
+	public Hilos3(int numero) {
+		this.numero = numero;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Hilos3 [numero=" + numero + "]";
+	}
+
+
+
+	
+	
+	public static void main(String[] args) {
+		// HAREMOS Que el hilo principal MAIN no termine 
+		//hasta que no terminen todos los hilos
+		//es decir va a esperar!!!
+		//USAREMOS EL JOIN
+		
+		//Lo mismo de antes pero para un array
+		Hilos3[]  vectorDeHilos= new Hilos3[5];
+		
+		///Instancio cada posición del array
+		for ( int i =0 ; i<vectorDeHilos.length; i++) {
+			
+			vectorDeHilos[i] = new Hilos3(i); 
+			//Instancio cada elemento del array
+			
+		}
+		
+		///Corro hilos para cara pos del vector, OBvio que se podria hacer en 
+		//el mismo for
+		for ( int i =0 ; i<vectorDeHilos.length; i++) {
+			
+			vectorDeHilos[i].start();
+			
+		}
+		
+		///Metodo join, necesita el try y catch, haremos que el main
+		//se ejecute siempre, despues del hilo 0
+		
+		try {
+			vectorDeHilos[0].join();
+		}catch(Exception ex){}
+		
+		
+		
+		System.out.println("Soy el main!!!");
+		
+
+	}
+
+}
