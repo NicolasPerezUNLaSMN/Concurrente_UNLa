ef8457813e3b1536fcf47e460709e07aace9c66e
NykolaiPerezUNLaSMN <nykolaiperez@gmail.com>
2024-03-05 15:37:58 -0300

    Ejemplo 1 Concurrencia Java - Hilos v1

---
+++ Ejemplo1/src/Ejemplos/EficienciaEnTiempos.java
@@ -0,0 +1,93 @@
+package Ejemplos;
+
+import java.util.Random;
+
+public class EficienciaEnTiempos extends Thread{
+	
+	
+	private static double[] vec = new double[80000000];
+	
+	
+	private int inicio, fin;
+	
+	public EficienciaEnTiempos(int inicio, int fin){
+		this.inicio = inicio;
+		this.fin = fin;
+	}
+	
+	
+	
+	public static void main(String[] args){
+		iniciavec();
+		
+		//opcion NO concurrente:
+		vec_NOconcurrente();
+		
+		//opcion concurrente:
+		vec_concurrente_muchos_hilos();
+		
+	
+	}
+	
+	//Metodo que inicia el vector estatico con valores aleatorios
+	private static void iniciavec(){
+		Random rand = new Random(System.nanoTime());
+		
+		for(int i = 0; i < vec.length; i++){
+			vec[i] = rand.nextInt();
+		}
+	}
+	
+	//Metodo que NO utiliza el paralelismo y por tanto se ejecuta de forma secuencial
+	private static void vec_NOconcurrente(){
+		double tiempo = System.nanoTime();
+		for(int i = 0; i < vec.length; i++){
+			vec[i] /= 10;
+			vec[i] *= 10;
+			vec[i] /= 10;
+		}
+		System.out.println("Version NO concurrente: "+ ((System.nanoTime() - tiempo) / 1000000) + " milisegundos");
+	}
+	
+	
+	
+	//Metodo que ejecuta los hilos que que se lanzan
+	public void run(){
+		for(int i = inicio; i < fin; i++){
+			vec[i] /= 10;
+			vec[i] *= 10;
+			vec[i] /= 10;
+		}
+	}
+	
+	//Metodo que ejecuta nproc hilos en paralelo y que llaman al metodo run para realizar la multiplicacion del
+	//vector de forma paralela
+	private static void vec_concurrente_muchos_hilos(){
+		int nproc = Runtime.getRuntime().availableProcessors(); //Devuelve cuantos nucleos tiene la CPU
+		int inicio = 0, fin = 0;
+		EficienciaEnTiempos[] prin = new EficienciaEnTiempos[nproc];
+		
+	
+		
+		double tiempo = System.nanoTime(); 	//Comienzo para capturar el tiempo que tarda en ejecutarse 
+											//este metodo
+		
+		for(int i = 0; i < nproc; i++){//Multiplicacion del vector por los nproc hilos
+			inicio = fin;
+			fin += vec.length/nproc;
+			prin[i] = new EficienciaEnTiempos(inicio, fin);
+			prin[i].start();
+		}
+		
+		for(int i = 0; i < nproc; i++){
+			try{
+				prin[i].join();
+			}catch(Exception e){}
+		}
+		System.out.println("Version Concurrente "+nproc +"Hilos:"+ ((System.nanoTime() - tiempo) / 1000000) + " milisegundos");
+	}	
+	
+	
+
+	
+}
---
+++ Ejemplo1/src/Ejemplos/Hilos1.java
@@ -0,0 +1,50 @@
+package Ejemplos;
+
+///Primero heredar la clase Hilos Thread
+public class Hilos1  extends Thread{
+	
+	//Lea grego un atributo cualquiera a mi clase Hilos1
+	public int numero; 
+	
+	//Obviamente genero su contructor
+	public Hilos1(int numero) {
+		this.numero = numero;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Hilos1 [numero=" + numero + "]";
+	}
+
+
+
+	////Luego sobreescribir el procedimiento run
+	//Este método es el que se lanza al inicar un nuevo hilo
+	public void run() {
+		System.out.println(toString());
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hilos1 h1 = new Hilos1(1);
+		Hilos1 h2 = new Hilos1(2);
+		Hilos1 h3 = new Hilos1(3);
+		
+		///Lanzamos el primer hilo, es decir inicia
+		//una instancia de la clase que hereda de Thread
+		h1.start();///Esto inicia lo que está en run O__O
+		h2.start();///Al crearse demora un tiempo
+		h3.start();
+		
+		///!No sabemos en que orden se ejecutaran los hilos
+		///el print no puede superponer texto por eso se ve
+		///un mensaje u otro aleatoreamente
+		
+		System.out.println("Soy el main!!!");
+		
+
+	}
+
+}
